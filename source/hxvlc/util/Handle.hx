bff18ae9e10fa458f8653d375d642088d04e79cc
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-02 21:51:25 +0200

    Update Handle.hx

--- source/hxvlc/util/Handle.hx
+++ source/hxvlc/util/Handle.hx
@@ -184,7 +184,7 @@ class Handle
 		    setupEnvVariables();
 
 	    	// ✅ Fix: explicitly pass 0 to StdVector constructor
-		    var args:cpp.StdVector<cpp.ConstCharStar> = new cpp.StdVector<cpp.ConstCharStar>();
+		    var args:cpp.StdVector<cpp.ConstCharStar> = new cpp.StdVector<cpp.ConstCharStar>(0);
 
 		    args.push_back("--aout=amem,none");
 		    args.push_back("--intf=none");
